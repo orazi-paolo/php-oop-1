2be8a6c411b2f0cb31d94abfcf5b89f28651b95a
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-09 15:45:57 +0200

    instantiate two objects

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 // includo la classe
 require_once '/Book.php';
+
+// istanzio due oggetti
+$firstBook = new Book('Atomic habits', 'James Clear', 2019);
+$secondBook = new Book('Il potere delle abitudini', 'Charles Duhigg', 2014);
 ?>
 
 <!DOCTYPE html>
